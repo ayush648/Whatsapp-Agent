7faeb2770038db75c21927b56a61eb9755bc793c
ayush <ayush648@gmail.com>
2026-05-08 05:54:01 +0530

    add

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["2eab-119-252-197-173.ngrok-free.app"],
+  serverExternalPackages: ["pdf-parse", "pdfjs-dist"],
 };
 
 export default nextConfig;
